ffa44109fd7b0c15fb9cd0b660ffc1d4cfafa925
Mark Mann <mann.brinson@gmail.com>
2021-03-24 13:06:54 -0700

    added head to home page

--- server.js
+++ server.js
@@ -11,10 +11,12 @@ app.set('json spaces', 2);
 // ROUTES
 app.get('/', function(req, res) {
 
-    ////  GOAL: For each list_type, construct url, hit the endpoint and get 30 items
+    ////  GOAL: For each list_type, construct url, hit the endpoint and get 20 items
     //// PARAMETERS
     var entity_types = ["movie", "tv"]
     var list_types = ["popular", "top_rated", "trending"]
+
+    var head_el_pair = ["movie", "now_playing"]
     var api_key = "2e510746ca28d7041056c7e57108de4c"
     // TODO: Get now_playing separately
     
@@ -68,14 +70,18 @@ app.get('/', function(req, res) {
     }
 
     //// DRIVER
+    //Body lists
     url_list = entity_listtypes.map(el_pair => {
         return buildURL(api_key, el_pair[0], el_pair[1])
     })
-    console.log(url_list)
-
     requests = url_list.map(url => {
         return buildReq(url)
     })
+
+    //Head list
+    url_head = buildURL(api_key, head_el_pair[0], head_el_pair[1])
+    request_head = buildReq(url_head)
+    requests.push(request_head) //IMPORTANT: The last request is for head
 
     //WORKING
     //Send requests asynchronously, then process all responses
@@ -84,31 +90,51 @@ app.get('/', function(req, res) {
         //For each response, for each movie object, parse out the desired features
         // based on list_type (ex: "now_playing", "trending", ...)
         var output = {}
+
+        //// BODY-LISTS
         responses.forEach((response, i) => {
-
-            entity_type = entity_listtypes[i][0]
-            list_type = entity_listtypes[i][1]
-            const movie_list_extracted = []
-            // console.log([entity_type, list_type]) //Works
-
-            //INNER LOOP
-            response.data.results.forEach((obj, j) => {
-                // console.log({j: [entity_type, list_type]}) //Works
-
-                result = extract_details(obj, entity_type, list_type)
-                // console.log(result) //Works
-
-                movie_list_extracted.push(result)
-            })
-            
-            //Associate the list_type with the extracted movies/tv_shows
-            var obj_list = new Object()
-            obj_list[list_type] = movie_list_extracted
-            if (!(entity_type in output)) {
-                output[entity_type] = [obj_list]
-            } else {
-                output[entity_type].push(obj_list)
+            if (i < responses.length-1) { //Only look at body responses
+                entity_type = entity_listtypes[i][0]
+                list_type = entity_listtypes[i][1]
+                const movie_list_extracted = []
+                // console.log([entity_type, list_type]) //Works
+    
+                //INNER LOOP
+                response.data.results.forEach((obj, j) => {
+                    if (j < 20) { //Grab the top 20 movies/shows
+                        result = extract_details(obj, entity_type, list_type)
+                        movie_list_extracted.push(result)
+                    }
+                })
+                
+                //Associate the list_type with the extracted movies/tv_shows
+                var obj_list = new Object()
+                obj_list[list_type] = movie_list_extracted
+                if (!(entity_type in output)) {
+                    output[entity_type] = [obj_list]
+                } else {
+                    output[entity_type].push(obj_list)
+                }
             }
+        //// HEAD-LIST
+        head_resp = responses[responses.length-1]
+
+        //Extract movie features
+        const movie_list_extracted = []
+        response.data.results.forEach((obj, i) => {
+            if (i < 5) { //Grab the top 5 movies/shows
+                entity_type = head_el_pair[0]
+                list_type = head_el_pair[1]
+                result = extract_details(obj, entity_type, list_type)
+                movie_list_extracted.push(result)
+            }
+        })
+
+        //Associate list_type with extracted movies
+        var obj_list = new Object()
+        obj_list[list_type] = movie_list_extracted
+        output["head"] = obj_list
+
         })
         res.send(output)
 
